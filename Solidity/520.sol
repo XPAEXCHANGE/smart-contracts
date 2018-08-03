c13523612067a043773e9e118fe1c3d18c851f38
ninolin <a7872278722@gmail.com>
2018-08-03 17:01:23 +0800

    Create 520.sol
    init 520.sol

---
+++ Solidity/520.sol
@@ -0,0 +1,181 @@
+pragma solidity ^0.4.24;
+
+interface Token {
+    function totalSupply() constant external returns (uint256 ts);
+    function balanceOf(address _owner) constant external returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) constant external returns (uint256 remaining);
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+interface transferRecipient {
+    function receiveTransfer(address _from, uint256 _value, bytes _extraData) external;
+}
+
+contract SafeMath {
+    function safeAdd(uint x, uint y)
+        internal
+        pure
+    returns(uint) {
+        uint256 z = x + y;
+        require((z >= x) && (z >= y));
+        return z;
+    }
+
+    function safeSub(uint x, uint y)
+        internal
+        pure
+    returns(uint) {
+        require(x >= y);
+        uint256 z = x - y;
+        return z;
+    }
+
+    function safeMul(uint x, uint y)
+        internal
+        pure
+    returns(uint) {
+        uint z = x * y;
+        require((x == 0) || (z / x == y));
+        return z;
+    }
+    
+    function safeDiv(uint x, uint y)
+        internal
+        pure
+    returns(uint) {
+        require(y > 0);
+        return x / y;
+    }
+
+    function random(uint N, uint salt)
+        internal
+        view
+    returns(uint) {
+        bytes32 hash = keccak256(abi.encodePacked(block.number, msg.sender, salt));
+        return uint(hash) % N;
+    }
+}
+
+contract LoveToken is SafeMath {
+    mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+    string public symbol = "";
+    string public name = "";
+
+    mapping(address => string[]) public userMsgs;
+    struct message{
+        address user;
+        string msg;
+        uint256 time;
+    }
+    message[] public allMsgs;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Issue(address indexed _to, uint256 indexed _value);
+
+    constructor (
+        string _symbol,
+        string _name
+    ) public payable {
+        symbol = _symbol;
+        name = _name;
+    }
+
+    /* Send coins */
+    function transfer(
+        address to_,
+        uint256 amount_
+    )
+        public
+    returns(bool success) {
+        if(balances[msg.sender] >= amount_ && amount_ > 0) {
+            balances[msg.sender] = safeSub(balances[msg.sender], amount_);
+            balances[to_] = safeAdd(balances[to_], amount_);
+            emit Transfer(msg.sender, to_, amount_);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* A contract attempts to get the coins */
+    function transferFrom(
+        address from_,
+        address to_,
+        uint256 amount_
+    ) public returns(bool success) {
+        if(balances[from_] >= amount_ && allowed[from_][msg.sender] >= amount_ && amount_ > 0) {
+            balances[to_] = safeAdd(balances[to_], amount_);
+            balances[from_] = safeSub(balances[from_], amount_);
+            allowed[from_][msg.sender] = safeSub(allowed[from_][msg.sender], amount_);
+            emit Transfer(from_, to_, amount_);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function balanceOf(
+        address _owner
+    )
+        constant
+        public
+    returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(
+        address _spender,
+        uint256 _value
+    )
+        public
+    returns (bool success) {
+        assert((_value == 0) || (allowed[msg.sender][_spender] == 0));
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /* Send coins and call receiveTransfer*/
+    function transferAndCall(address _to, uint256 _value, bytes _extraData) public returns (bool success) {
+        if(transfer(_to,  _value)) {
+            transferRecipient(_to).receiveTransfer(msg.sender, _value, _extraData);
+            return true;
+        }
+    }
+
+    function allowance(address _owner, address _spender) constant public returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    function issue(
+        address user_,
+        uint256 amount_
+    )
+        internal
+    returns(bool success) {
+        if(amount_ > 0 && user_ != address(0)) {
+            totalSupply = safeAdd(totalSupply, amount_);
+            balances[user_] = safeAdd(balances[user_], amount_);
+            emit Issue(user_, amount_);
+            emit Transfer(this, user_, amount_);
+            return true;
+        }
+    }
+
+    function sendLoveMessage(string _msg)
+        public  
+    {
+       issue(msg.sender, 520 ether);
+       userMsgs[msg.sender].push(_msg);
+       allMsgs.push(message(msg.sender, _msg, block.timestamp ));
+    }
+}
